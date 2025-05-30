263a2e86733c49d7accfb70d02a031bc17f0ad3c
EllieB-BelliE <ellie_eb@yahoo.com>
2025-05-30 19:58:04 +0100

    Data Sanitization

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,13 +1,12 @@
 import random
-cpu_options = ["Rock","Paper","Scissors"]
+cpu_options = ["rock","paper","scissors"]
 
 def main():
     print("Welcome to RockPaperScissors!\n")
-    print("This is my branch!\n")
 
-    user_choice = input("Pick one Rock, Paper, or Scissors.\n> ")
+    user_choice = None
     while user_choice not in cpu_options:
-        user_choice = input("Please enter a valid option from the list, Rock, Paper ,Scissors\n> ")
+        user_choice = input("Please enter an option from the list, Rock, Paper ,Scissors\n> ").lower()
     cpu_choice = random.choice(cpu_options)
 
     print("You chose " + user_choice + ", I chose " + cpu_choice)
