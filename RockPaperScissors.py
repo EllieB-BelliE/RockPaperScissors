718e463864aa5cd01c3d336a71da5149828b6f19
EllieB-BelliE <ellie_eb@yahoo.com>
2025-05-30 18:47:55 +0100

    Added basic Rock Paper Scissors format

---
+++ RockPaperScissors.py
@@ -0,0 +1,29 @@
+import random
+
+print("Welcome to RockPaperScissors!\n")
+
+user_choice = input("Pick one Rock, Paper, or Scissors.\n> ")
+
+cpu_options = ["Rock","Paper","Scissors"]
+cpu_choice = random.choice(cpu_options)
+
+print("You chose " + user_choice + ", I chose " + cpu_choice)
+if cpu_choice == user_choice:
+    print ("Tie!")
+elif cpu_choice == 'Rock':
+    if user_choice == 'Paper':
+        print("You win!")
+    else: 
+        print("You lose!")
+
+elif cpu_choice == 'Paper':
+    if user_choice == 'Rock':
+        print("You lose!")
+    else:
+        print("You win!")
+
+else:
+    if user_choice == 'Rock':
+        print("You win")
+    else:
+        print("You lose")
